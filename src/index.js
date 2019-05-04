4c44435d45e2f00c40033078ddcec2ba49e2febc
Linus Miller <lohfu@lohfu.io>
2019-05-04 13:14:16 +0200

    Update deps and build settings

--- src/index.js
+++
@@ -1,4 +0,0 @@
-export { default as actionsMany } from './actions-many'
-export { default as actionsSingle } from './actions-single'
-export { default as reducerMany } from './reducer-many'
-export { default as reducerSingle } from './reducer-single'
---
+++ src/index.mjs
@@ -0,0 +1,4 @@
+export { default as actionsMany } from './actionsMany'
+export { default as actionsSingle } from './actionsSingle'
+export { default as reducerMany } from './reducerMany'
+export { default as reducerSingle } from './reducerSingle'
